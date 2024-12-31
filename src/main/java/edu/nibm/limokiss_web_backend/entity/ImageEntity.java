270a8222998bc1ca82137e78863073023bf9900a
YUKI41442 <dresslyclothingcompany.com@gmail.com>
2024-12-31 11:10:44 +0530

    init Commit

--- src/main/java/edu/nibm/limokiss_web_backend/entity/ImageEntity.java
+++ src/main/java/edu/nibm/limokiss_web_backend/entity/ImageEntity.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "images")
 public class ImageEntity {
+    ///
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
